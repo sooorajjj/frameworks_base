a42e9db0aac42b186f532d2a09ec751c27acbb23
Andrew Boren <andrew.boren@gmail.com>
2015-03-24 19:17:52 +0100

    Powermenu screenshot delay [1/2]
     * Only works from power menu
     * Defaults to 1 second (1000ms)
    
    Change-Id: Ic7477d4ed962b97d5bbfd980cfdb7e3516262463

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -3699,6 +3699,12 @@ public final class Settings {
         public static final String LOCKSCREEN_ENABLE_POWER_MENU = "lockscreen_enable_power_menu";
 
         /**
+         * Change the screenshot delay
+         * @hide
+         */
+        public static final String SCREENSHOT_DELAY = "screenshot_delay";
+
+        /**
          * Settings to backup. This is here so that it's in the same place as the settings
          * keys and easy to update.
          *
